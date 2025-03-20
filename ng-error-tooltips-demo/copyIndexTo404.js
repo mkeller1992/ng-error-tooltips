feffd1520ef41d3135638f1e8511db01d6acf381
Matthias Keller <matthias.b.keller@bluewin.ch>
2025-03-20 17:13:57 +0100

    Changed build output folder

--- ng-error-tooltips-demo/copyIndexTo404.js
+++ ng-error-tooltips-demo/copyIndexTo404.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcPath = path.join(__dirname, 'dist', 'ng-error-tooltips-demo', 'index.html');
-const destPath = path.join(__dirname, 'dist', 'ng-error-tooltips-demo', '404.html');
+const srcPath = path.join(__dirname, 'dist', 'ng-error-tooltips-demo', 'browser', 'index.html');
+const destPath = path.join(__dirname, 'dist', 'ng-error-tooltips-demo','browser', '404.html');
 
 fs.copyFile(srcPath, destPath, (err) => {
   if (err) throw err;

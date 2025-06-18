b076744e5492c2e679722e18d61c8930139736a7
Matthias Keller <matthias.b.keller@bluewin.ch>
2025-06-18 11:36:39 +0200

    Updated configuration of jest-unit-tests for demo-project

--- ng-error-tooltips-demo/jest.config.js
+++ ng-error-tooltips-demo/jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
     collectCoverage: true,
     coverageDirectory: 'coverage',
     collectCoverageFrom: [
-      '<rootDir>/src/app/*.{ts,js}',
-      '!<rootDir>/src/app/*.routes.ts', // Exclude route-files
+        '<rootDir>/src/app/*.{ts,js}',
+        '!<rootDir>/src/app/*.routes.ts', // Exclude route-files
     ],
     coverageReporters: ['html', 'text', 'text-summary', 'lcov'],
+    moduleNameMapper: {
+      '^@ng-error-tooltips$': '<rootDir>/../ng-error-tooltips-lib/projects/ng-error-tooltips/src/public-api.ts'
+    },
+    resolver: 'jest-ts-webcompat-resolver'
 };

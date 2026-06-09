4b55ab66d57aa280dec15b1c3a0bef4bc9d2ff29
Matthias Keller <matthias.b.keller@bluewin.ch>
2026-06-09 21:24:24 +0200

    Allow static language values in error tooltip provider

--- ng-error-tooltips-lib/projects/ng-error-tooltips/src/lib/provide-error-tooltips.ts
+++ ng-error-tooltips-lib/projects/ng-error-tooltips/src/lib/provide-error-tooltips.ts
@@ -1,13 +1,29 @@
-import { makeEnvironmentProviders, Signal, signal } from '@angular/core';
+import { isSignal, makeEnvironmentProviders, Signal, signal } from '@angular/core';
+
 import { ERROR_TOOLTIP_LANG } from './error-tooltip-lang.token';
-import { SupportedLanguage } from './validators/supported-language.type';
 import { ERROR_TOOLTIP_SIG_VALIDATE, SignalValidateFn } from './error-tooltip-sig-validate.token';
+import { SupportedLanguage } from './validators/supported-language.type';
 
-export function provideErrorTooltips(opts: { lang?: Signal<SupportedLanguage>; validate?: SignalValidateFn }) {
-  const langSig = opts.lang ?? signal<SupportedLanguage>('de');
+export interface ErrorTooltipProviderOptions {
+	lang?: SupportedLanguage | Signal<SupportedLanguage>;
+	validate?: SignalValidateFn;
+}
 
-  return makeEnvironmentProviders([
-    { provide: ERROR_TOOLTIP_LANG, useValue: langSig },
-    ...(opts.validate ? [{ provide: ERROR_TOOLTIP_SIG_VALIDATE, useValue: opts.validate }] : []),
-  ]);
+export function provideErrorTooltips(options: ErrorTooltipProviderOptions = {}) {
+	const langSig: Signal<SupportedLanguage> = isSignal(options.lang)
+		? options.lang
+		: signal(options.lang ?? 'de');
+
+	return makeEnvironmentProviders([
+		{
+			provide: ERROR_TOOLTIP_LANG,
+			useValue: langSig,
+		},
+		...(options.validate
+			? [{
+				provide: ERROR_TOOLTIP_SIG_VALIDATE,
+				useValue: options.validate,
+			}]
+			: []),
+	]);
 }
